93282b70a5e2f8260a18149539c7b3b57ee705f3
Scott Wright <scottwright.qrf@gmail.com>
2022-03-23 20:22:33 -0500

    changes

--- src/components/TopNavbar/Nav.styled.js
+++ src/components/TopNavbar/Nav.styled.js
@@ -10,7 +10,7 @@ export const StyledNav = styled.nav`
     justify-content: right;
     align-items: center;
     font-size: 1px;
-    position: fixed;
+    position: relative;
     top: 0;
     z-index: 1;
     padding: 15px;
